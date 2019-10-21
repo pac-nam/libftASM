b236d18005cc91944f6344f2a18a067a028fb1a8
pacman <tbleuse@student..42.fr>
2019-10-21 16:09:30 +0200

    fd changed for puttrucs

--- tests_unitaires/test_puttrucs.c
+++ tests_unitaires/test_puttrucs.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <tbleuse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:54:40 by tbleuse           #+#    #+#             */
-/*   Updated: 2019/10/21 15:16:33 by tbleuse          ###   ########.fr       */
+/*   Updated: 2019/10/21 16:07:23 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ void		ft_test_puttrucs(void)
 	ft_putchar('u');
 	ft_putchar('t');
 	ft_putchar_fd('c', 1);
-	ft_putchar_fd('h', 1);
-	ft_putchar_fd('a', 1);
+	ft_putchar_fd('h', 2);
+	ft_putchar_fd('a', 2);
 	ft_putstr("r test end 0 error detected\n");
 	ft_putstr_fd("putchar_fd test end 0 error detected\n", 1);
 	ft_putstr("putstr  test end 0 error detected");
 	ft_putendl("");
-	ft_putstr_fd("putstr_fd test end 0 error detected", 1);
-	ft_putendl_fd("", 1);
+	ft_putstr_fd("putstr_fd test end 0 error detected", 2);
+	ft_putendl_fd("", 2);
 	ft_putendl("putendl test end 0 error detected");
 	ft_putendl_fd("putendl_fd test end 0 error detected", 1);
 	ft_putstr(NULL);
