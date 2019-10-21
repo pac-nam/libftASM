58541c4280b166dc4406e3259e9c7b5d3b149ce8
pacman <tbleuse@student..42.fr>
2019-10-21 16:04:52 +0200

    strdup test modified

--- tests_unitaires/test_strdup.c
+++ tests_unitaires/test_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <tbleuse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 16:26:36 by tbleuse           #+#    #+#             */
-/*   Updated: 2019/10/18 10:30:48 by tbleuse          ###   ########.fr       */
+/*   Updated: 2019/10/21 16:03:55 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void		ft_test_strdup(void)
 
 	mine = ft_strdup("");
 	sys = strdup("");
-	if (strcmp(sys, mine))
+	if (strcmp(sys, mine) || mine == sys || mine == &("hello"[0]))
 	{
 		printf("error 1 strdup: sys |%s| != mine |%s|\n", sys, mine);
 		++error;
@@ -36,7 +36,7 @@ void		ft_test_strdup(void)
 	strdel(sys);
 	mine = ft_strdup("hello");
 	sys = strdup("hello");
-	if (strcmp(sys, mine))
+	if (strcmp(sys, mine) || mine == sys || mine == &("hello"[0]))
 	{
 		printf("error 2 strdup: sys |%s| != mine |%s|\n", sys, mine);
 		++error;
